db389920e6a460d713a15b5f30b266452b70e390
Addison-Stavlo <AddieStavlo@gmail.com>
2019-01-16 17:38:15 -0500

    cohort endpoints complete, mvp complete

--- api/cohortsRoute.js
+++ api/cohortsRoute.js
@@ -3,7 +3,14 @@ const router = express.Router();
 const db = require('./dbConfig');
 
 //middleware functions
-
+function checkBody (req,res,next) {
+    if(req.body.name){
+        next();
+    }
+    else{
+        res.status(400).json({message: 'please add a name field to the request'})
+    }
+}
 //endpoints
 router.get('/', (req,res,next) => {
     db('cohorts')
@@ -40,12 +47,54 @@ router.get('/:id/students', (req,res,next) => {
         .catch(err => next(err));
 })
 
-//error handler
+router.post('/', checkBody, (req,res,next) => {
+    db('cohorts').insert(req.body)
+        .then(ids => {
+            db('cohorts').where({id: ids[0]})
+                .then(cohort => {
+                    res.status(201).json({message: 'successfully created cohort',cohort})
+                })
+        })
+        .catch(err => next(err))
+})
+
+router.put('/:id', checkBody, (req,res,next) => {
+    db('cohorts').where({id: req.params.id}).update(req.body)
+        .then(count => {
+            if(count){
+                db('cohorts').where({id: req.params.id})
+                    .then(cohort => {
+                        res.status(200).json({message: `successfully updated cohort`, cohort})
+                    })
+            }
+            else{
+                res.status(404).json({message: `cohort of id ${req.params.id} not found`})
+            }
+        })
+        .catch(err=> next(err))
+})
+
+router.delete('/:id', async (req,res,next) => {
+    try{
+        let cohort = await db('cohorts').where({id: req.params.id});
+        if(cohort.length){
+            await db('cohorts').where({id: req.params.id}).del();
+            res.status(200).json({message: 'successfully deleted cohort', cohort: cohort[0]})
+        }
+        else{
+            res.status(404).json({message: `cohort of id ${req.params.id} not found`})
+        }
+    }
+    catch(err){
+        next(err);
+    }
+})
+//catch error handler
 router.use( (err,req,res,next) => {
 if(err.errno === 19){
     res.status(500).json({message: `name already used`})
     }
-    else {
+else {
     res.status(500).json(err) 
     }    
 });
